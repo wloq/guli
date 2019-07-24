71b32b5188c0517a804382e3cda2b3f99bcca538
wloq <499274855@qq.com>
2019-07-24 19:48:42 +0800

    修改1

--- apps/users/models.py
+++ apps/users/models.py
@@ -31,4 +31,11 @@ class BannerInfo(models.Model):
 class EmailVerifyCode(models.Model):
     code = models.CharField(max_length=20,verbose_name='邮箱验证码')
     email = models.EmailField(max_length=200,verbose_name='验证码邮箱')
-    
+    send_type = models.IntegerField(choices=((1,'register'),(2,'forget'),(3,'change')),verbose_name='验证码类型')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = "邮箱验证码信息"
+        verbose_name_plural = verbose_name
+    def __str__(self):
+        return self.code
